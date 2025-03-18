c72314bc1bb799e1a406c8cccecc545d98006538
opilane <opilane@HP-CZC2349HV2>
2025-03-18 10:01:45 +0200

    def

--- Kasutajate_funktsioonide_loomine.py
+++ Kasutajate_funktsioonide_loomine.py
@@ -1,3 +1,4 @@
+from tkinter import N
 from module1_def import *
 #arithmetic funktsiooni kasutamine
 a=float(input("Sisesta arv 1:"))
@@ -5,3 +6,21 @@ b=float(input("Sisesta arv 2:"))
 t=input("Tehe: ")
 v=arithmetic(a,b,t)
 print(v)
+
+#is_year_leap funktsiooni kasutamine
+aasta=int(input("Mis aasta tahad kontrollida? "))
+if aasta>0: 
+    v=is_year_leap(aasta)
+    print(v)
+    if v:
+        print(f"{aasta} on liigiaasta")
+    else:
+        print(f"{aasta} ei ole liigiaasta")
+
+#square funktsiooni kasutamine
+S,P,d=square(float(input("Sisesta külg: ")))
+print(f"{S}\n, {P}\n, {d}\n")
+
+#square_list funktsiooni kasutamine
+S,P,d=square_list(float(input("Sisesta külg: ")))
+print(f"{S}\n, {P}\n, {d}\n")
--- module1_def.py
+++ module1_def.py
@@ -24,3 +24,36 @@ def is_year_leap(aasta:int)->bool:
     :param int aasta : aasta number
     :rtype:bool tagastab tõeväärsuses formaadis tulemus
     """
+    if aasta%4==0:
+        v=True
+    else:
+        v=False
+    return v
+
+def square(külg: float) -> any:
+    """Square
+    S=külg**2
+    P=külg*4
+    d=(2)**(1/2)*külg
+    :param float külg: Sisend kasutajalt, mingi ujukomaarv
+    :rtype: var märgib tüüpi str
+    """
+    S=külg**2
+    P=külg*4
+    d=(2)**(1/2)*külg
+    return S,P,d
+
+def square_list(külg: float) -> list:
+    """Square
+    s_list=[S,P,d] where 
+    S=külg**2
+    P=külg*4
+    d=(2)**(1/2)*külg
+    :param float külg: Sisend kasutajalt, mingi ujukomaarv
+    :rtype: var märgib tüüpi str
+    """
+    S=külg**2
+    P=külg*4
+    d=(2)**(1/2)*külg
+    s_list=[S,P,d]
+    return S,P,d
