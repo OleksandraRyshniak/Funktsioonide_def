f5b9870a4c46f13057a6e77d9718846661d52e55
opilane <opilane@HP-CZC2349HV2>
2025-03-19 09:57:03 +0200

    def _

--- Kasutajate_funktsioonide_loomine.py
+++ Kasutajate_funktsioonide_loomine.py
@@ -1,26 +1,37 @@
-from tkinter import N
 from module1_def import *
-#arithmetic funktsiooni kasutamine
-a=float(input("Sisesta arv 1:"))
-b=float(input("Sisesta arv 2:"))
-t=input("Tehe: ")
-v=arithmetic(a,b,t)
+# #arithmetic funktsiooni kasutamine
+# a=float(input("Sisesta arv 1:"))
+# b=float(input("Sisesta arv 2:"))
+# t=input("Tehe: ")
+# v=arithmetic(a,b,t)
+# print(v)
+
+# #is_year_leap funktsiooni kasutamine
+# aasta=int(input("Mis aasta tahad kontrollida? "))
+# if aasta>0: 
+# #     v=is_year_leap(aasta)
+# #     print(v)
+# #     if v:
+# #         print(f"{aasta} on liigiaasta")
+# #     else:
+# #         print(f"{aasta} ei ole liigiaasta")
+
+# #square funktsiooni kasutamine
+# S,P,d=square(float(input("Sisesta külg: ")))
+# print(f"{S},\n{P},\n{d}\n")
+
+# #square_list funktsiooni kasutamine
+# S,P,d=square_list(float(input("Sisesta külg: ")))
+# print(f"{S}\n, {P}\n, {d}\n")
+
+# kuu=seasonInput
+# print(kuu)
+
+# a=float(input("Sisesta summa: "))
+# b=int(input("Sisesta years: "))
+# v=bank(a,b)
+# print(v)
+
+arv=int(input("Sisesta arv: "))
+v=is_prime(arv)
 print(v)
-
-#is_year_leap funktsiooni kasutamine
-aasta=int(input("Mis aasta tahad kontrollida? "))
-if aasta>0: 
-    v=is_year_leap(aasta)
-    print(v)
-    if v:
-        print(f"{aasta} on liigiaasta")
-    else:
-        print(f"{aasta} ei ole liigiaasta")
-
-#square funktsiooni kasutamine
-S,P,d=square(float(input("Sisesta külg: ")))
-print(f"{S}\n, {P}\n, {d}\n")
-
-#square_list funktsiooni kasutamine
-S,P,d=square_list(float(input("Sisesta külg: ")))
-print(f"{S}\n, {P}\n, {d}\n")
--- module1_def.py
+++ module1_def.py
@@ -1,3 +1,6 @@
+#1
+
+
 def arithmetic(arv1:float, arv2:float, tehe:str)->any:
     """Lihtne kalkulaator
     + - liitmine
@@ -17,7 +20,7 @@ def arithmetic(arv1:float, arv2:float, tehe:str)->any:
     else:
         vastus="Tundmatu tehe"
     return vastus
-
+#2
 def is_year_leap(aasta:int)->bool:
     """Liigaasta leidmine
     Tagastab True, kui liigaasta ja False Kui on tavaline aasta.
@@ -29,7 +32,7 @@ def is_year_leap(aasta:int)->bool:
     else:
         v=False
     return v
-
+#3
 def square(külg: float) -> any:
     """Square
     S=külg**2
@@ -57,3 +60,69 @@ def square_list(külg: float) -> list:
     d=(2)**(1/2)*külg
     s_list=[S,P,d]
     return S,P,d
+
+#4 
+def season(kuu_number:int) -> str:
+    """Aastaajad
+    Hooaja tagastamine (talv, kevad, suvi või sügis).
+    :param int kuu_number : Sisend kasutajalt
+    :rtype: var märgib tüüpi str
+    """
+    if kuu_number==1 or 2 or 12:
+       kuu="talv"
+    elif kuu_number==3 or 4 or 5:
+       kuu="kevad"
+    elif kuu_number==6 or 7 or 8:
+       kuu2="suvi"
+    else:
+       kuu3="sügis"
+    return kuu
+
+
+def seasonInput()->str:
+    """Aastaaja leidmine
+    Tagastab aastaaja nine
+    :rtype str tagastab aastaja nine
+    """
+    k=int(input("Sisesta kuu number: "))
+    while True:
+        if k in range(1,13):
+            break
+        else:
+            k=int(input("Sisesta kuu number: "))
+    return season(k)
+
+#5
+def bank(aeur:float, years:int)->float:
+    """ Panga Deposiit
+    Tagastab summa, mis on kasutaja kontol.
+    :param float aeur, Sisend kasutajalt
+    :param int years, Sisend kasutajalt
+    :rtype float 
+    """
+    for i in range(years):
+        aeur=aeur*1,10
+    return aeur
+
+#6
+def is_prime(arv:int)->bool:
+    """ Primaarvud 
+    Tagastab True, kui see on lihtne, ja False, kui see ei ole.
+    :param int arv, Sisend kasutajalt
+    :rtype bool tagastab tõeväärsuses formaadis tulemus
+    """
+    if 0<=arv<1001:
+        if arv in [0,1]:
+            pass
+        else:
+            for i in range (2, arv):
+                    if arv%i==0:
+                        return False
+                    else:
+                        return True
+
+#7
+def date(päev:int, kuu:int, aasta:int) -> bool:
+    """
+    """
+
